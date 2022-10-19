99dea4c6ad19a67c780e1e1aeff29fdfef1db3b1
Aaron Love <dev.aaronlove@gmail.com>
2022-10-19 09:21:12 -0600

    LoanForm Component Functionality

--- src/LoanForm.jsx
+++ src/LoanForm.jsx
@@ -3,12 +3,33 @@ import React from "react";
 class LoanForm extends React.Component {
   constructor() {
     super();
-    this.state = { loanAmount: 1000, rate: 3 }
+    this.state = { loanAmount: 1000, rate: 3.0 }
   }
+
+  updateLoan = ({target: {value}}) => this.setState({loanAmount: value})
+  updateRate = ({target: {value}}) => this.setState({rate: value})
 
   render() {
     return(
-      <div>test LoanForm</div>
+      <div>
+        <label htmlFor="loanAmount">Loan Amount</label>
+        <input 
+          onChange={this.updateLoan}
+          type="number"
+          min="1000" 
+          step="100"
+          value={this.state.loanAmount} 
+        />
+        <label htmlFor="interestRate">Interest Rate (Annual)</label>
+        <input 
+          onChange={this.updateRate}
+          type="number"
+          min="0.1"
+          max="100" 
+          step="0.1"
+          value={this.state.rate} 
+        />
+      </div>
     )
   }
 }
